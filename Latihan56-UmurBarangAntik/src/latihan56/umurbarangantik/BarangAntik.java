982a41f7fc38eb62baba380c534b0af40457705a
Evanlaksana <132762464+Evanlaksana@users.noreply.github.com>
2023-11-30 11:57:09 +0700

    Add files via upload

---
+++ Latihan56-UmurBarangAntik/src/latihan56/umurbarangantik/BarangAntik.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan56.umurbarangantik;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BarangAntik {
+    int umur;
+    
+    public BarangAntik(int umur){
+      this.umur = umur;  
+    }
+    
+    public void tampilUmur(){
+        System.out.println("Umur Barang Antik ini adalah : " +umur+ " tahun ");
+        
+    }
+    
+}
